282c5b58b8b98b7cc5a596626a7488af0af2963b
eGalv3zDev <acamposeco-2024018@kinal.edu.gt>
2025-09-21 11:56:47 -0600

    feat/FacturaService

---
+++ kinalcoffeeshop/src/main/java/org/asco_devs/kinalcoffeeshop/dominio/service/FacturaService.java
@@ -0,0 +1,38 @@
+package org.asco_devs.kinalcoffeeshop.dominio.service;
+
+import org.asco_devs.kinalcoffeeshop.dominio.dto.FacturaDto;
+import org.asco_devs.kinalcoffeeshop.dominio.dto.ModFacturaDto;
+import org.asco_devs.kinalcoffeeshop.dominio.dto.ModProductoDto;
+import org.asco_devs.kinalcoffeeshop.dominio.repository.FacturaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FacturaService {
+    private final FacturaRepository facturaRepository;
+
+    public FacturaService(FacturaRepository facturaRepository) {
+        this.facturaRepository = facturaRepository;
+    }
+
+    public List<FacturaDto> obtenerFacturas() {
+        return this.facturaRepository.obtenerFacturas();
+    }
+
+    public FacturaDto buscarFacturaPorId(Long idFactura) {
+        return this.facturaRepository.buscarFacturaPorId(idFactura);
+    }
+
+    public FacturaDto guardarFactura(FacturaDto dto) {
+        return this.facturaRepository.guardarFactura(dto);
+    }
+
+    public FacturaDto modificarFactura(Long idFactura, ModFacturaDto mod) {
+        return this.facturaRepository.modificarFactura(idFactura, mod);
+    }
+
+    public void eliminarFactura(Long idFactura) {
+        this.facturaRepository.eliminarFactura(idFactura);
+    }
+}
