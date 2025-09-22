9b434c4733b8d3c2d0fd6df2d0e937a740e158fb
AADLCC <adelacruz-2021326@kinal.edu.gt>
2025-09-21 21:13:57 -0600

    CRUD de LineasdeCredito Agregada

---
+++ kinalcoffeeshop/src/main/java/org/asco_devs/kinalcoffeeshop/dominio/service/LineaDeCreditoService.java
@@ -0,0 +1,37 @@
+package org.asco_devs.kinalcoffeeshop.dominio.service;
+
+import org.asco_devs.kinalcoffeeshop.dominio.dto.LineaDeCreditoDto;
+import org.asco_devs.kinalcoffeeshop.dominio.dto.ModLineaDeCreditoDto;
+import org.asco_devs.kinalcoffeeshop.dominio.repository.LineaDeCreditoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LineaDeCreditoService {
+    private final LineaDeCreditoRepository repository;
+
+    public LineaDeCreditoService(LineaDeCreditoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<LineaDeCreditoDto> obtenerLineasDeCredito() {
+        return this.repository.obtenerLineasDeCredito();
+    }
+
+    public LineaDeCreditoDto buscarPorId(Long idConsumo) {
+        return this.repository.buscarPorId(idConsumo);
+    }
+
+    public LineaDeCreditoDto guardarLineaDeCredito(LineaDeCreditoDto dto) {
+        return this.repository.guardarLineaDeCredito(dto);
+    }
+
+    public LineaDeCreditoDto modificarLineaDeCredito(Long idConsumo, ModLineaDeCreditoDto mod) {
+        return this.repository.modificarLineaDeCredito(idConsumo, mod);
+    }
+
+    public void eliminarLineaDeCredito(Long idConsumo) {
+        this.repository.eliminarLineaDeCredito(idConsumo);
+    }
+}
